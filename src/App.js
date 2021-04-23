25ec66a782798387c0fb7866a86dfcea43cc5be9
Anirudha Bose <anirudha.bose@alumni.cern>
2021-04-23 13:38:10 +0530

    Add headings to price tracker and watchlist

--- src/App.js
+++ src/App.js
@@ -8,11 +8,14 @@ function App() {
   return (
     <div className="grid grid-flow-col grid-rows-1 grid-cols-3 gap-4">
       <div className="col-start-1 col-span-2">
+      <h1 class="text-xl border-b mb-2">Today's Cryptocurrency Prices by Market Cap</h1>
+
         <Coins />
         <Pagination />
       </div>
 
       <div className="col-start-3 col-span-1">
+      <h1 class="text-xl border-b mb-2">Watchlist</h1>
         <WatchList />
       </div>
     </div>
